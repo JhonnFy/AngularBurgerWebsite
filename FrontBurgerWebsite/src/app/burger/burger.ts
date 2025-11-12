434482b188df24d8c2336337210de515b6d689fc
JhonFy <moesDeveloper1023@gmail.com>
2025-11-11 21:55:53 -0500

    [ng generate component burger]

---
+++ FrontBurgerWebsite/src/app/burger/burger.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-burger',
+  imports: [],
+  templateUrl: './burger.html',
+  styleUrl: './burger.scss',
+})
+export class Burger {
+
+}
