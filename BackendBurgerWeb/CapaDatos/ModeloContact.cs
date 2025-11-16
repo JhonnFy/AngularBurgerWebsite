e3f853132aad0e955ca30251216f813e29d73c8e
JhonFy <moesDeveloper1023@gmail.com>
2025-11-15 21:57:50 -0500

    [Backend].[5].[Modelo Contact].[Terminado]

--- BackendBurgerWeb/CapaDatos/ModeloContact.cs
+++ BackendBurgerWeb/CapaDatos/ModeloContact.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class ModeloContact
+    public class ModeloContact
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+
+        public ModeloContact()
+        {
+            id = 0;
+            name = string.Empty;
+            address = string.Empty;
+            phone = string.Empty;
+        }
+
     }
 }
