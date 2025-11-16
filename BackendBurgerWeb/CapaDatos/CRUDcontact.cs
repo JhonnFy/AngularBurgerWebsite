a362c8591d31079ec64bebf324625092ffb2cf7c
JhonFy <moesDeveloper1023@gmail.com>
2025-11-16 01:46:37 -0500

    [Backend].[1].[Metodos CRUD].[ReadContact()].[OK]

---
+++ BackendBurgerWeb/CapaDatos/CRUDcontact.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CRUDcontact
+    {
+
+        private Conexion conexion = new Conexion();
+
+        public List<ModeloContact> ReadContact()
+        {
+            var ListaReadContact = new List<ModeloContact>();
+
+            try
+            {
+                using (var db = conexion.ObtenerCadenaDeConexion())
+                {
+                    db.Open();
+                    string @read =
+                        "SELECT * FROM contact";
+
+                    using (SqlCommand readSelect = new SqlCommand(read, db))
+                    using (SqlDataReader runReadSelect = readSelect.ExecuteReader())
+                        while (runReadSelect.Read())
+                        {
+                            var modelo = new ModeloContact
+                            {
+                                id = runReadSelect.GetInt64(0),
+                                name = runReadSelect.GetString(1),
+                                address = runReadSelect.GetString(2),
+                                phone = runReadSelect.GetInt64(3)
+                            };
+                            ListaReadContact.Add(modelo);
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[****].[ERROR].[CRUDcontact].[ReadContact]" + ex.Message);
+                throw new Exception("[****].[ERROR].[CRUDcontact].[ReadContact]" + ex.Message);
+            }
+            return ListaReadContact;
+        }
+    }
+}
