7076bc26c15fdcebb26fe041382a34e2aec347df
JhonFy <moesDeveloper1023@gmail.com>
2025-11-15 21:42:25 -0500

    [Backend].[4].[Modelo Clients].[Terminado]

--- BackendBurgerWeb/CapaDatos/ModeloClients.cs
+++ BackendBurgerWeb/CapaDatos/ModeloClients.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class ModeloClients
+    public class ModeloClients
     {
+        public string cc { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string reference { get; set; }
+        public string payment_method { get; set; }
+
+        public ModeloClients()
+        {
+            cc = string.Empty;
+            name = string.Empty;
+            address = string.Empty;
+            phone1 = string.Empty;
+            phone2 = string.Empty;
+            reference = string.Empty;
+            payment_method = string.Empty;
+        }
+
     }
 }
