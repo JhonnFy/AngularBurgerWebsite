34aa8b3f2d43ca6392ef3adb5cad5e853171ee16
JhonFy <moesDeveloper1023@gmail.com>
2025-11-15 22:55:03 -0500

    [Backend].[7].[Modelo order_About].[Terminado]

--- BackendBurgerWeb/CapaDatos/ModeloOrderAbout.cs
+++ BackendBurgerWeb/CapaDatos/ModeloOrderAbout.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class ModeloOrderAbout
+    public class ModeloOrderAbout
     {
+        public int id { get; set; }
+        public string client_cc { get; set; } /*Fk*/
+        public int hamburger_id { get; set; }
+        public int quantity { get; set; }
+        public decimal total_price { get; set; }
+        public string status { get; set; }
+        public int store_id { get; set; }/* FK*/
+        public DateTime created_at { get; set; }
+
+
+        public ModeloOrderAbout()
+        {
+            id = 0;
+            client_cc = string.Empty;
+            hamburger_id = 0;
+            quantity = 0;
+            total_price = 0;
+            status = string.Empty;
+            store_id = 0;
+            created_at = DateTime.Now;
+        }
     }
 }
