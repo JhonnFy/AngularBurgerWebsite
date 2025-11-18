3e31fa95fefa00f0652f9fea97daae05a76043cb
JhonFy <moesDeveloper1023@gmail.com>
2025-11-17 22:58:23 -0500

    [Backend].[14].[Capa Datos].[CRUDcontact].[Test PostContac].[OK]

---
+++ BackendBurgerWeb/CapaTesting/Testing CapaDatos/Test PostContac.cs
@@ -0,0 +1,68 @@
+﻿using CapaDatos;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+
+    [TestClass]
+    public class Test_PostContac
+    {
+        private Conexion conexion = new Conexion();
+
+        [TestMethod]
+        public void TestPostContact()
+        {
+            try
+            {
+                ModeloContact newContact = new ModeloContact
+                {
+                    id = 4,
+                    name = "Test contact",
+                    address = "tc 16 av 10",
+                    phone = 3105447722
+                };
+
+                CRUDcontact metodContact = new CRUDcontact();
+                bool runSql = metodContact.PostContac(newContact);
+
+
+                var contact = metodContact.GetContactsId((int)newContact.id);
+                if (contact != null  && contact.Any())
+                {
+                    Debug.WriteLine("[****].[OK].[Capa Testing].[La Creación Del Registro Fue Exitosa.]");
+                    Debug.WriteLine($"Id: {contact.First().id}");
+                    Debug.WriteLine($"Name: {contact.First().name}");
+                    Debug.WriteLine($"Address: {contact.First().address}");
+                    Debug.WriteLine($"Phone: {contact.First().phone}");
+                    
+                    Console.WriteLine("[****].[OK].[Capa Testing].[La Creación Del Registro Fue Exitosa.]");
+                    Console.WriteLine($"Id: {contact.First().id}");
+                    Console.WriteLine($"Name: {contact.First().name}");
+                    Console.WriteLine($"Address: {contact.First().address}");
+                    Console.WriteLine($"Phone: {contact.First().phone}");
+                    Assert.IsTrue(runSql, "[****].[OK].[Capa Testing].[La Creación Del Registro Fue Exitosa.]");
+                }
+                else
+                {
+                    Debug.WriteLine("[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado]");
+                    Console.WriteLine("[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado]");
+                    Assert.Fail("[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado]");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado]" + ex.Message);
+                Console.WriteLine("[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado]" + ex.Message);
+                throw new Exception($"[****].[INFO].[CapaDatos].[Test POSTContact].[PostContac].[Contac NO Registrado] " + ex.Message);
+            }
+        }
+    }
+}
