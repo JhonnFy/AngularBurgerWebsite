d35ee7eb2d8db5ab8a3649e0a9c76b196aa2260a
JhonFy <moesDeveloper1023@gmail.com>
2025-11-18 16:27:08 -0500

    [Backend].[20].[Capa Datos].[CRUDmenuHamburger].[Test GetMenuHamburger].[OK]

---
+++ BackendBurgerWeb/CapaTesting/Testing CapaDatos/Test GetMenuHamburger.cs
@@ -0,0 +1,49 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    [TestClass]
+    public class Test_GetMenuHamburger
+    {
+        [TestMethod]
+        public void TestGetMenuHamburger()
+        {
+            try
+            {
+                CRUDmenuHamburger objGetMenuHamburger = new CRUDmenuHamburger();
+                var MenuHamburger = objGetMenuHamburger.GetMenuHamburger();
+
+                if (MenuHamburger == null)
+                {
+                    Assert.Fail("[****].[Error].[EL Metodo GetMenuHamburger, Fallo]");
+                    Debug.WriteLine("[****].[Error].[EL Metodo GetMenuHamburger, Fallo]");
+                    Console.WriteLine("[****].[Error].[EL Metodo GetMenuHamburger, Fallo]");
+                }else
+                {
+                    Debug.WriteLine($"[****].[TestOk].[TotalRegistros] {MenuHamburger.Count}");
+                    Console.WriteLine($"[****].[TestOk].[TotalRegistros] {MenuHamburger.Count}");
+
+                    int i = 0;
+                    while(i < MenuHamburger.Count)
+                    {
+                        Debug.WriteLine("[****].[TestGetMenuHamburger].[OK] " + MenuHamburger[i].name);
+                        Console.WriteLine("[****].[TestGetMenuHamburger].[OK] " + MenuHamburger[i].name);
+                        i++;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[****].[ERROR].[TestGetMenuHamburger] " + ex.Message);
+                throw new Exception("[****].[ERROR].[TestGetMenuHamburger] " + ex.Message);
+            }
+        }
+    }
+}
