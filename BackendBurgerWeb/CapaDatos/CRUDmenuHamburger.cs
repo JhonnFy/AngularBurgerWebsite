81e505d8cb67235df9409beea538bb65f0779436
JhonFy <moesDeveloper1023@gmail.com>
2025-11-18 16:51:09 -0500

    [Backend].[21].[Capa Datos].[CRUDmenuHamburger].[GetMenuHamburgerId].[OK]

--- BackendBurgerWeb/CapaDatos/CRUDmenuHamburger.cs
+++ BackendBurgerWeb/CapaDatos/CRUDmenuHamburger.cs
@@ -14,8 +14,47 @@ namespace CapaDatos
 {
     public class CRUDmenuHamburger
     {
-
         private Conexion conexion = new Conexion();
+
+        public List<ModeloMenuHamburger> GetMenuHamburgerId(int id)
+        {
+            var ListaGetMenuHamburgerId = new List<ModeloMenuHamburger>();
+
+            try
+            {
+                using (var db = conexion.ObtenerCadenaDeConexion())
+                {
+                    db.Open();
+                    string @Read =
+                        "SELECT * FROM menuHamburger " +
+                        "WHERE ID = @id";
+
+                    using (SqlCommand readSelect = new SqlCommand(Read, db))
+                    {
+                        readSelect.Parameters.AddWithValue("@id", id);
+
+                        using (SqlDataReader runReadSelect = readSelect.ExecuteReader())
+                        {
+                            while (runReadSelect.Read())
+                            {
+                                var modelo = new ModeloMenuHamburger
+                                {
+                                    id = runReadSelect.GetInt64(0),
+                                    name = runReadSelect.GetString(1)
+                                };
+                                ListaGetMenuHamburgerId.Add(modelo);
+                            }
+                        }
+                    }
+                }
+            }catch (Exception ex)
+            {
+                Debug.WriteLine("[****].[ERROR].[CRUDcontact].[Test].[GetMenuHamburgerId]" + ex.Message);
+                throw new Exception("[****].[ERROR].[CRUDcontact].[Test].[GetMenuHamburgerId]" + ex.Message);
+            }
+            return ListaGetMenuHamburgerId;
+        }
+
 
         public List<ModeloMenuHamburger> GetMenuHamburger()
         {
