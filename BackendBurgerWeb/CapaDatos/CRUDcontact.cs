14bc7a5aef1ea26c4dc01ebb726b44bfb47f5a1c
JhonFy <moesDeveloper1023@gmail.com>
2025-11-20 04:51:48 -0500

    [Backend GetContact()].[1].[Capa Datos].[Se Crea El Metodo: GetContact()]

--- BackendBurgerWeb/CapaDatos/CRUDcontact.cs
+++ BackendBurgerWeb/CapaDatos/CRUDcontact.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
@@ -14,6 +15,57 @@ namespace CapaDatos
 {
     public class CRUDcontact
     {
+        private Conexion conexion = new Conexion();
 
+        public List<ModeloContact> GetContact()
+        {
+            var listaGetContact = new List<ModeloContact>();
+
+            try
+            {
+                using (var db = conexion.ObtenerCadenaDeConexion())
+                {
+                    db.Open();
+                    string @Read =
+                        "SELECT id, name, address, phone FROM contact";
+
+                    using (SqlCommand objSqlCommand = new SqlCommand(Read, db))
+                    using (SqlDataReader objSqlDataReader = objSqlCommand.ExecuteReader())
+                    {
+                        while (objSqlDataReader.Read())
+                        {
+                            var modeloContact = new ModeloContact
+                            {
+                                id = objSqlDataReader.GetInt64(0),
+                                name = objSqlDataReader.GetString(1),
+                                address = objSqlDataReader.GetString(2),
+                                phone = objSqlDataReader.GetInt64(3)
+                            };
+                            listaGetContact.Add(modeloContact);
+
+                            Debug.WriteLine($@"
+                                    Id: {modeloContact.id}
+                                    Name: {modeloContact.name}
+                                    Address: {modeloContact.address}
+                                    Phone: {modeloContact.phone}
+                            ");
+
+                            Console.WriteLine($@"
+                                    Id: {modeloContact.id}
+                                    Name: {modeloContact.name}
+                                    Address: {modeloContact.address}
+                                    Phone: {modeloContact.phone}
+                            ");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[CRUDcontact.GetContacts] " +  ex.Message);
+                throw new Exception("[CRUDcontact.GetContacts] " + ex.Message);
+            }
+            return listaGetContact;
+        }
     }
 }
