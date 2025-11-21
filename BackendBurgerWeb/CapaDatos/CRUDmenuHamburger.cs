e4880759ac19a597097e620425d3d295ed105964
JhonFy <moesDeveloper1023@gmail.com>
2025-11-21 03:57:56 -0500

    [Backend GetMenuHamburger].[1].[Capa Datos].[CRUDcontact.cs].[Se Crea El Metodo GetMenuHamburger].[OK]

--- BackendBurgerWeb/CapaDatos/CRUDmenuHamburger.cs
+++ BackendBurgerWeb/CapaDatos/CRUDmenuHamburger.cs
@@ -14,8 +14,40 @@ namespace CapaDatos
 {
     public class CRUDmenuHamburger
     {
-       
+        private Conexion conexion = new Conexion();
 
+        public List<ModeloMenuHamburger> GetMenuHamburger()
+        {
+            var listMenuHamburger = new List<ModeloMenuHamburger>();
 
+                try
+                {
+                    using (var db = conexion.ObtenerCadenaDeConexion())
+                    {
+                        db.Open();
+                    string @Get =
+                        "SELECT id, name FROM menuHamburger";
+                    using (SqlCommand objSqlCommand = new SqlCommand(Get, db))
+                    using (SqlDataReader objSqlDataReader = objSqlCommand.ExecuteReader())
+                        {
+                            while (objSqlDataReader.Read())
+                            {
+                                var modeloMenuHamburger = new ModeloMenuHamburger
+                                {
+                                    id = objSqlDataReader.GetInt64(0),
+                                    name = objSqlDataReader.GetString(1)
+                                };
+                            listMenuHamburger.Add(modeloMenuHamburger);
+                            }
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine("[****].[Error].[CRUDmenuHamburger.GetHamburger] " + ex.Message);
+                    throw new Exception("[****].[Error].[CRUDmenuHamburger.GetHamburger] " + ex.Message);
+                }
+            return listMenuHamburger;
+        }
     }
 }
