9646ca7c3238fa4ee2f36f75587643a9f94827bb
JhonFy <moesDeveloper1023@gmail.com>
2025-11-21 11:27:26 -0500

    [Backend Test DeleteMenuHamburger].[5.1].[Capa Datos].[CRUDcontact.cs].[Se Crea El Test DeleteMenuHamburger].[OK]

---
+++ BackendBurgerWeb/CapaTesting/Testing MenuHamburger/Test DeleteMenuHamburger.cs
@@ -0,0 +1,56 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_MenuHamburger
+{
+    [TestClass]
+    public class Test_DeleteMenuHamburger
+    {
+        [TestMethod]
+        public void TestDeleteMenuHamburger()
+        {
+
+            CRUDmenuHamburger objCrudMenu = new CRUDmenuHamburger();
+            var MetodMenu = objCrudMenu.GetMenuHamburgerId(6);
+
+            try
+            {
+                if (MetodMenu != null && MetodMenu.Count>0)
+                {
+                    foreach (var lista in MetodMenu)
+                    {
+                        lista.id = 6;
+                        objCrudMenu.DeleteMenuHamburger(lista);
+
+                        Debug.WriteLine($@"
+                                [Debug Desde Test]
+                                Id Eliminado: {lista.id}
+                            ");
+
+                        Console.WriteLine($@"
+                                [Debug Desde Test]
+                                Id Eliminado: {lista.id}
+                            ");
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("[****].[Registro No Eliminado]");
+                    Assert.Fail("[****].[Registro No Eliminado]");
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("[****].[Registro No Eliminado]");
+                Assert.Fail("[****].[Registro No Eliminado]");
+            }
+
+        }
+
+    }
+}
