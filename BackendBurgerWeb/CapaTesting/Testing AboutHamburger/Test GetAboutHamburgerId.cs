408643ca87c4115b2726f4f23fa48498bbf4b27a
JhonFy <moesDeveloper1023@gmail.com>
2025-11-21 23:42:09 -0500

    [Backend Test].[2.1].[Capa Datos].[CRUDaboutHamburger].[Se Crea El Test GetAboutHamburgerId].[OK]

---
+++ BackendBurgerWeb/CapaTesting/Testing AboutHamburger/Test GetAboutHamburgerId.cs
@@ -0,0 +1,44 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_AboutHamburger
+{
+    [TestClass]
+    public class Test_GetAboutHamburgerId
+    {
+        [TestMethod]
+        public void TestGetAboutHamburgerId()
+        {
+            CRUDaboutHamburger objCrud = new CRUDaboutHamburger();
+            var MetodAbout = objCrud.GetAboutHamburgerId(100);
+
+            try
+            {
+                foreach (var metodAbout in MetodAbout)
+                {
+                    Debug.WriteLine($@"
+                            [Debug Test]
+                            Id: {metodAbout.id},
+                            Name: {metodAbout.name}
+                        ");
+
+                    Console.WriteLine($@"
+                            [Console Test]
+                            Id: {metodAbout.id},
+                            Name: {metodAbout.name}
+                        ");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("[****].[Error].[TestGetAboutHamburgerId]" + ex.Message);
+                Assert.Fail("[****].[Error].[TestGetAboutHamburgerId]" + ex.Message);
+            }
+        }
+    }
+}
