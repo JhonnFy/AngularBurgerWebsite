5708c24b305e81a391f9d27f0d3d28ee86bf29d9
JhonFy <moesDeveloper1023@gmail.com>
2025-11-21 20:40:30 -0500

    [Backend].[1].[Capa Datos].[CRUDaboutHamburger].[Se Crea El Metodo GetAboutHamburger].[OK]

---
+++ BackendBurgerWeb/CapaDatos/CRUDaboutHamburger.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CRUDaboutHamburger
+    {
+        Conexion conexion = new Conexion();
+
+        public List<ModeloAboutHamburger> GetAboutHamburger()
+        {
+            var listaModeloAbout = new List<ModeloAboutHamburger>();
+
+            try
+            {
+                using (var db = conexion.ObtenerCadenaDeConexion())
+                {
+                    db.Open();
+                    string @Get =
+                        "SELECT id, name FROM AboutHamburger ";
+
+                    using (SqlCommand objSqlCommand = new SqlCommand(Get, db))
+                    using (SqlDataReader objData = objSqlCommand.ExecuteReader())
+                    {
+                        while (objData.Read())
+                        {
+                            var Modelo = new ModeloAboutHamburger
+                            {
+                                id = objData.GetInt64(0),
+                                name = objData.GetString(1)
+                            };
+                            listaModeloAbout.Add(Modelo);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener los datos de AboutHamburger: " + ex.Message);
+            }
+            return listaModeloAbout;
+        }
+    }
+}
