8fad77115264fc35e21a7766c50b351492e3d3d9
JhonFy <moesDeveloper1023@gmail.com>
2025-11-24 00:10:26 -0500

    [Backend].[4].[Capa Datos].[CRUDaboutHamburger].[Se Crea El Metodo PutAboutHamburger].[OK]

--- BackendBurgerWeb/CapaDatos/CRUDaboutHamburger.cs
+++ BackendBurgerWeb/CapaDatos/CRUDaboutHamburger.cs
@@ -72,8 +72,6 @@ namespace CapaDatos
         }
         
 
-
-
         public List<ModeloAboutHamburger> GetAboutHamburgerId(int id)
         {
             var listModeloAbout = new List<ModeloAboutHamburger>();
