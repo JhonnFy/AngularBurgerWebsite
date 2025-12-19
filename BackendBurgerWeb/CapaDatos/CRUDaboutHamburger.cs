44b3fba730f92dc207d4c74d0a25709f192ff230
JhonFy <moesDeveloper1023@gmail.com>
2025-12-19 03:35:43 -0500

    [Create].[Se Crea El Metodo: GetAboutHamburgers]

--- BackendBurgerWeb/CapaDatos/CRUDaboutHamburger.cs
+++ BackendBurgerWeb/CapaDatos/CRUDaboutHamburger.cs
@@ -15,7 +15,37 @@ namespace CapaDatos
     {
         Conexion conexion = new Conexion();
 
-
-
+        public List<ModeloAboutHamburger> GetAboutHamburgers()
+        {
+            var list = new List<ModeloAboutHamburger>();
+            
+            try
+            {
+                using (var db = conexion.ObtenerCadenaDeConexion())
+                {
+                    db.Open();
+                    using (var cmd = new SqlCommand("GetAboutHamburger", db))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                list.Add(new ModeloAboutHamburger
+                                {
+                                    id = reader.GetInt64(reader.GetOrdinal("id")),
+                                    name = reader.IsDBNull(reader.GetOrdinal("name")) ? "" : reader.GetString(reader.GetOrdinal("name"))
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("[****].[Error].[GetAboutHamburgers]" + ex.Message);
+            }
+            return list;
+        }
     }
 }
